498951e786daac87755f22695136ab67ce1571be
HatziCaspeta <95260655+HatziCaspeta@users.noreply.github.com>
2022-12-12 01:34:48 -0600

    Update main.py

--- Pagina/html/main.py
+++ Pagina/html/main.py
@@ -11,7 +11,7 @@ mysql=MySQL(app)
 
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'Zeecpoeultion9077!'
+app.config['MYSQL_PASSWORD'] = ''
 app.config['MYSQL_DB'] = 'smilecare'
 
 app.secret_key = 'cl1n1c4d3nt4l'
